b0979f41fea06733d17b1ceb826596e40b2bdcf2
ahmad <ahmadalmawa7@gmail.com>
2026-04-29 22:35:09 +0530

    fix

--- .next/static/s4bGYWpOcT2YxHGPYgODI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/18-a0d8bcc7559714c1.js","static/chunks/pages/index-22277e1da2578fe8.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
